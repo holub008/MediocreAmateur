9997d8612b680017fe384cedf6f0a69d60147247
Karl Holub <holub008@umn.edu>
2019-08-12 02:59:03 -0500

    adding a 400 page

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use('/', express.static('../www'));
 
 app.get('*', (req, res) => {
     res.status(400);
-    res.send('Could not find that file!');
+    res.sendFile(path.join(__dirname, '../www/not_found.html'));
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
